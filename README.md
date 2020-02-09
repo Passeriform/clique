b8c0fcd4c9599a995658647508b3e3f8f4eb46cc
Utkarsh Bhardwaj <utbh23041999@gmail.com>
2020-02-10 04:35:10 +0530

    Create an almost complete visualizer

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # clique
-Damn simple classification visualizer
+Damn simple regression visualizer (for now).
+
+The idea is to make points-based visualizations as abstractly approachable as possible.
---
+++ main.js
@@ -0,0 +1,28 @@
+var cliqueVis = new CliqueVisualizer(new visualConfig(5), [])
+
+function setup()
+{
+	cliqueVis.init('cliqueholder')
+}
+
+function draw()
+{
+	cliqueVis.setBackground()
+  cliqueVis.drawPoints()
+	cliqueVis.drawAddons()
+	cliqueVis.showHUD()
+}
+
+function mousePressed()
+{
+	if (event.target.classList.contains('p5Canvas'))
+		cliqueVis.modPoints(mouseButton, mouseX, mouseY)
+}
+
+function windowResized(){
+	cliqueVis.recalculateWindow(windowWidth, windowHeight)
+}
+
+function dispatchAction(event) {
+	cliqueVis.handleDispatch(event)
+}
